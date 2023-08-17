d6321ee2fa592b1c2952d24072cfc8f4a37f6341
Mauro Vera <Maurovera069@gmail.com>
2023-08-17 01:05:07 -0300

    Layout de las pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,17 +6,21 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Auth Nextjs13',
-  description: 'Autenticacion con Nextjs 13',
+  description: 'Autenticacion con Nextjs 13'
 }
 
-export default function RootLayout({
-  children,
-}: {
+interface RoutLayoutProps {
   children: React.ReactNode
-}) {
+}
+
+export default function RootLayout ({ children }: RoutLayoutProps) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={inter.className}>
+        <main className='h-screen flex flex-col items-center justify-center'>
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
