4fb61d965cb7f89061f0a5da019219c6ca7438fa
Mauro Vera <Maurovera069@gmail.com>
2023-08-18 10:59:28 -0300

    Conecte a la api estos formularios

--- src/components/Forms/ChangePasswordForm.tsx
+++ src/components/Forms/ChangePasswordForm.tsx
@@ -1,27 +1,53 @@
 'use client'
 
+import { useAuthFetch } from '@/hooks/useAuthFetch'
+import { useSearchParams } from 'next/navigation'
 import { Form } from '@/components/Form'
+import { AxiosRequestConfig } from 'axios'
 
-export const ChangePasswordForm = () => (
-  <Form
-    title='Cambia tu contraseña'
-    description='Formulario para cambiar tu contraseña'
-    onSubmit={() => console.log('change pass form submit')}
-  >
-    <div className='my-[10px] flex flex-col gap-4'>
-      <Form.Input
-        placeholder='Ingresa tu nueva contraseña...'
-        label='Contraseña'
-        name='password'
-        type='password'
-      />
-      <Form.Input
-        placeholder='Repite tu contraseña...'
-        label='Confirmar contraseña'
-        name='confirmPassword'
-        type='password'
-      />
-    </div>
-    <Form.SubmitButton buttonText='Cambiar contraseña' />
-  </Form>
-)
+export const ChangePasswordForm = () => {
+  const searchParams = useSearchParams()
+
+  const authFetch = useAuthFetch()
+
+  const changePassword = async (formData: any) => {
+    const token = searchParams.get('token')
+
+    const options: AxiosRequestConfig<any> = {
+      headers: {
+        token
+      }
+    }
+
+    // Agregar redirect a la Home cuando este la page
+    await authFetch({
+      endpoint: 'change-password',
+      formData,
+      options
+    })
+  }
+
+  return (
+    <Form
+      title='Cambia tu contraseña'
+      description='Formulario para cambiar tu contraseña'
+      onSubmit={changePassword}
+    >
+      <div className='my-[10px] flex flex-col gap-4'>
+        <Form.Input
+          placeholder='Ingresa tu nueva contraseña...'
+          label='Contraseña'
+          name='newPassword'
+          type='password'
+        />
+        <Form.Input
+          placeholder='Repite tu contraseña...'
+          label='Confirmar contraseña'
+          name='confirmPassword'
+          type='password'
+        />
+      </div>
+      <Form.SubmitButton buttonText='Cambiar contraseña' />
+    </Form>
+  )
+}
--- src/components/Forms/ForgetPasswordForm.tsx
+++ src/components/Forms/ForgetPasswordForm.tsx
@@ -1,20 +1,30 @@
 'use client'
 
+import { useAuthFetch } from '@/hooks/useAuthFetch'
 import { Form } from '@/components/Form'
 
-export const ForgetPasswordForm = () => (
-  <Form
-    title='Recuperar contraseña'
-    description='Formulario para recuperar tu contraseña'
-    onSubmit={() => console.log('forget pass form submit')}
-  >
-    <div className='my-[10px] flex flex-col gap-4'>
-      <Form.Input
-        placeholder='Ingresa tu correo...'
-        label='Correo'
-        name='email'
-      />
-    </div>
-    <Form.SubmitButton buttonText='Recuperar contraseña' />
-  </Form>
-)
+export const ForgetPasswordForm = () => {
+  const authFetch = useAuthFetch()
+
+  const forgetPassword = async (formData: any) => {
+    // Agregar redirect a la Home cuando este la page
+    await authFetch({ endpoint: 'forget-password', formData })
+  }
+
+  return (
+    <Form
+      title='Recuperar contraseña'
+      description='Formulario para recuperar tu contraseña'
+      onSubmit={forgetPassword}
+    >
+      <div className='my-[10px] flex flex-col gap-4'>
+        <Form.Input
+          placeholder='Ingresa tu correo...'
+          label='Correo'
+          name='email'
+        />
+      </div>
+      <Form.SubmitButton buttonText='Recuperar contraseña' />
+    </Form>
+  )
+}
