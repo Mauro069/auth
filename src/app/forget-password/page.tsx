c6bff56ce5982b1bb5dba0b84ed93335765a0e45
Mauro Vera <Maurovera069@gmail.com>
2023-08-18 16:11:45 -0300

    Elimine los componentes de cada formulario y los deje en su page

--- src/app/forget-password/page.tsx
+++ src/app/forget-password/page.tsx
@@ -1,5 +1,37 @@
-import { ForgetPasswordForm } from '@/components/Forms/ForgetPasswordForm'
+'use client'
+
+import { useAuthFetch } from '@/hooks/useAuthFetch'
+import { useLoading } from '@/hooks/useLoading'
+import { Form } from '@/components/Form'
 
 export default function ForgetPasswordPage () {
-  return <ForgetPasswordForm />
+  const { isLoading, startLoading, finishLoading } = useLoading()
+  const authFetch = useAuthFetch()
+
+  const forgetPassword = async (formData: any) => {
+    startLoading()
+    await authFetch({ endpoint: 'forget-password', formData })
+    finishLoading()
+  }
+
+  return (
+    <Form
+      title='Recuperar contraseña'
+      description='Formulario para recuperar tu contraseña'
+      onSubmit={forgetPassword}
+    >
+      <div className='my-[10px] flex flex-col gap-4'>
+        <Form.Input
+          placeholder='Ingresa tu correo...'
+          label='Correo'
+          name='email'
+        />
+      </div>
+      <Form.SubmitButton
+        buttonText='Recuperar contraseña'
+        isLoading={isLoading}
+      />
+      <Form.Footer description='Volver al' textLink='Inicio' link='/' />
+    </Form>
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,44 @@
-import { LoginForm } from '../components/Forms/LoginForm'
+'use client'
+
+import { useAuthFetch } from '@/hooks/useAuthFetch'
+import { useLoading } from '@/hooks/useLoading'
+import { Form } from '@/components/Form'
 
 export default function LoginPage () {
-  return <LoginForm />
+  const { isLoading, startLoading, finishLoading } = useLoading()
+  const authFetch = useAuthFetch()
+
+  const login = async (formData: any) => {
+    startLoading()
+    await authFetch({ endpoint: 'login', redirectRoute: '/home', formData })
+    finishLoading()
+  }
+
+  return (
+    <Form
+      title='Inicia Sesión'
+      description='Formulario para iniciar sesión'
+      onSubmit={login}
+    >
+      <div className='my-[10px] flex flex-col gap-4'>
+        <Form.Input
+          placeholder='Ingresa tu correo...'
+          label='Correo'
+          name='email'
+        />
+        <Form.Input
+          placeholder='Ingresa tu contraseña...'
+          label='Contraseña'
+          name='password'
+          type='password'
+        />
+      </div>
+      <Form.SubmitButton buttonText='Iniciar Sesión' isLoading={isLoading} />
+      <Form.Footer
+        description='Ya tienes cuenta?'
+        textLink='Inicia Sesión'
+        link='/'
+      />
+    </Form>
+  )
 }
