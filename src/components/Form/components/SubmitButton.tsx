5c2aa864162c0908af812afbca9526f96a92d544
Mauro Vera <Maurovera069@gmail.com>
2023-08-18 15:31:58 -0300

    Agregue propiedad isLoading al submitButton

--- src/components/Form/components/SubmitButton.tsx
+++ src/components/Form/components/SubmitButton.tsx
@@ -1,13 +1,15 @@
+import { Loader } from '@/components/Loader'
 import styles from './styles.module.scss'
 
 interface SubmitButtonProps {
   buttonText: string
+  isLoading?: boolean
 }
 
-export function SubmitButton ({ buttonText }: SubmitButtonProps) {
+export function SubmitButton ({ buttonText, isLoading }: SubmitButtonProps) {
   return (
-    <button className={styles.submitButton} type='submit'>
-      {buttonText}
+    <button className={styles.submitButton} type='submit' disabled={isLoading}>
+      {isLoading ? <Loader /> : buttonText}
     </button>
   )
 }
