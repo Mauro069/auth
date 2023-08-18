34391d27c8112faa977048307c92f45cf279183e
Mauro Vera <Maurovera069@gmail.com>
2023-08-18 01:12:20 -0300

    Conectamos a la api estos formularios

--- src/components/Forms/LoginForm.tsx
+++ src/components/Forms/LoginForm.tsx
@@ -1,26 +1,36 @@
 'use client'
 
+import { useAuthFetch } from '@/hooks/useAuthFetch'
 import { Form } from '@/components/Form'
 
-export const LoginForm = () => (
-  <Form
-    title='Inicia Sesión'
-    description='Formulario para iniciar sesión'
-    onSubmit={() => console.log('login form submit')}
-  >
-    <div className='my-[10px] flex flex-col gap-4'>
-      <Form.Input
-        placeholder='Ingresa tu correo...'
-        label='Correo'
-        name='email'
-      />
-      <Form.Input
-        placeholder='Ingresa tu contraseña...'
-        label='Contraseña'
-        name='password'
-        type='password'
-      />
-    </div>
-    <Form.SubmitButton buttonText='Iniciar Sesión' />
-  </Form>
-)
+export const LoginForm = () => {
+  const authFetch = useAuthFetch()
+
+  const login = async (formData: any) => {
+    // Agregar redirect a la Home cuando este la page
+    await authFetch({ endpoint: 'login', formData })
+  }
+
+  return (
+    <Form
+      title='Inicia Sesión'
+      description='Formulario para iniciar sesión'
+      onSubmit={login}
+    >
+      <div className='my-[10px] flex flex-col gap-4'>
+        <Form.Input
+          placeholder='Ingresa tu correo...'
+          label='Correo'
+          name='email'
+        />
+        <Form.Input
+          placeholder='Ingresa tu contraseña...'
+          label='Contraseña'
+          name='password'
+          type='password'
+        />
+      </div>
+      <Form.SubmitButton buttonText='Iniciar Sesión' />
+    </Form>
+  )
+}
--- src/components/Forms/RegisterForm.tsx
+++ src/components/Forms/RegisterForm.tsx
@@ -1,32 +1,41 @@
 'use client'
 
+import { useAuthFetch } from '@/hooks/useAuthFetch'
 import { Form } from '@/components/Form'
 
-export const RegisterForm = () => (
-  <Form
-    title='Registrate'
-    description='Formulario para crear una cuenta'
-    onSubmit={() => console.log('register form submit')}
-  >
-    <div className='my-[10px] flex flex-col gap-4'>
-      <Form.Input
-        placeholder='Ingresa tu correo...'
-        label='Correo'
-        name='email'
-      />
-      <Form.Input
-        placeholder='Ingresa tu contraseña...'
-        label='Contraseña'
-        name='password'
-        type='password'
-      />
-      <Form.Input
-        placeholder='Repite tu contraseña...'
-        label='Confirmar contraseña'
-        name='confirmPassword'
-        type='password'
-      />
-    </div>
-    <Form.SubmitButton buttonText='Crear cuenta' />
-  </Form>
-)
+export const RegisterForm = () => {
+  const authFetch = useAuthFetch()
+
+  const register = async (formData: any) => {
+    await authFetch({ endpoint: 'register', formData, redirectRoute: '/' })
+  }
+
+  return (
+    <Form
+      title='Registrate'
+      description='Formulario para crear una cuenta'
+      onSubmit={register}
+    >
+      <div className='my-[10px] flex flex-col gap-4'>
+        <Form.Input
+          placeholder='Ingresa tu correo...'
+          label='Correo'
+          name='email'
+        />
+        <Form.Input
+          placeholder='Ingresa tu contraseña...'
+          label='Contraseña'
+          name='password'
+          type='password'
+        />
+        <Form.Input
+          placeholder='Repite tu contraseña...'
+          label='Confirmar contraseña'
+          name='confirmPassword'
+          type='password'
+        />
+      </div>
+      <Form.SubmitButton buttonText='Crear cuenta' />
+    </Form>
+  )
+}
